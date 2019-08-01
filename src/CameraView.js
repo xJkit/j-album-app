1ec5370fc9855e9557ec2098b1dbc3c22559d06f
xJkit-iMac <xjkit54780@gmail.com>
2019-08-02 03:17:06 +0800

    Refactor Camera with render Props

--- src/CameraView.js
+++ src/CameraView.js
@@ -1,15 +1,30 @@
 import React, { useRef } from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
 function CameraView(props) {
-  const camera = useRef(null);
   return (
     <View style={{ flex: 1 }}>
       <RNCamera
-        ref={camera}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
         style={{ flex: 1 }}
-      />
+      >
+        {({ camera, status }) => {
+          if (status !== 'READY') {
+            return (
+              <Text style={{ color: 'black' }}>請打開相機</Text>
+            );
+          }
+          return (
+            <>
+              <TouchableOpacity onPress={() => this.takePicture(camera)}>
+                <Text style={{ fontSize: 14 }}> SNAP </Text>
+              </TouchableOpacity>
+            </>
+          )
+        }}
+      </RNCamera>
     </View>
   );
 }
